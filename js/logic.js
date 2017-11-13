9755d27df40466bcbb7a7e6acf037a4dd565cc04
Matthew <matthewjames.ns@googlemail.com>
2017-11-13 17:50:45 -0400

    block adding core functionality implementation on schedule

--- js/logic.js
+++ js/logic.js
@@ -4,15 +4,50 @@ var studentList = [];
 /* Test Students */
 var testStudent1 = {
 	name: 'Matthew',
+	blockCounter: 0,
 	scheduleBlockList: []
 };
 
-/* Schedule Blocks for Students */
-populateScheduleBlocks(testStudent1.scheduleBlockList);
+/* On load */
+window.onload = populateSchedule;
 
-console.log(testStudent1.name);
-for (var i = 0; i < testStudent1.scheduleBlockList.length; i++) {
-	console.log(testStudent1.scheduleBlockList[i].location);
+function populateSchedule () {
+	populateScheduleBlocks(testStudent1.scheduleBlockList);
+	studentList.push(testStudent1);
+	drawBlock(testStudent1);
+}
+
+function toggleSchedule(name) {
+
+	var clickedName = name.replace(/\n|<.*?>/g,'');
+	var counter = 0;
+
+	console.log(clickedName + counter);
+
+	while(document.getElementById(clickedName + counter) != null) {
+		if (document.getElementById(clickedName + counter).style.visibility === 'hidden' )
+			document.getElementById(clickedName + counter).style.visibility = "visible";
+		if (document.getElementById(clickedName + counter).style.visibility === 'visble')
+			document.getElementById(clickedName+ counter).style.visibility = "hidden";
+		counter++;
+	}
+}
+
+/* Test Draw Block Logic */
+function drawBlock(student) {
+		var el =  document.createElement("div")
+		el.id = student.name + student.blockCounter;
+		el.innerHTML = "<p>" + student.name + "<p>";
+		document.getElementById("schedule-container").appendChild(el);
+
+		var newEl = document.getElementById(student.name + student.blockCounter);
+		student.blockCounter++;
+		newEl.classList.add('block-hour');
+		newEl.classList.add('block-box-blue');
+
+		newEl.style.top = "62px";
+		newEl.style.left = "70px";
+		newEl.style.visibility = "hidden";
 }
 
 /* Populate Test Student */
@@ -24,6 +59,7 @@ function populateScheduleBlocks(scheduleBlockList) {
 	block['name'] = "Cool Class 1";
 	block['id'] = "COOL1234";
 	block['location'] = "Goldberg";
+	block['day'] = "Monday";
 
 	scheduleBlockList.push(block);
 
@@ -34,6 +70,7 @@ function populateScheduleBlocks(scheduleBlockList) {
 	block['name'] = "Cool Class 1";
 	block['id'] = "COOL1234";
 	block['location'] = "Killam";
+	block['day'] = "Monday";
 
 	scheduleBlockList.push(block);
 
@@ -44,6 +81,7 @@ function populateScheduleBlocks(scheduleBlockList) {
 	block['name'] = "Cool Class 1";
 	block['id'] = "COOL1234";
 	block['location'] = "Dunn";
+	block['day'] = "Monday";
 
 	scheduleBlockList.push(block);
 
@@ -54,6 +92,7 @@ function populateScheduleBlocks(scheduleBlockList) {
 	block['name'] = "Cool Class 1";
 	block['id'] = "COOL1234";
 	block['location'] = "Mona Campbell";
+	block['day'] = "Monday";
 
 	scheduleBlockList.push(block);
 }
