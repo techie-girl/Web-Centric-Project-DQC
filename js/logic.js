55279b331701ae5d463a64b9ad8060f195519e82
Matthew <matthewjames.ns@googlemail.com>
2017-11-13 15:36:41 -0400

    line check

--- js/logic.js
+++ js/logic.js
@@ -3,12 +3,17 @@ var studentList = [];
 
 /* Test Students */
 var testStudent1 = {
-	name: 'Matthew';
-	scheduleBlockList = [];
-}
+	name: 'Matthew',
+	scheduleBlockList: []
+};
 
 /* Schedule Blocks for Students */
 populateScheduleBlocks(testStudent1.scheduleBlockList);
+
+console.log(testStudent1.name);
+for (var i = 0; i < testStudent1.scheduleBlockList.length; i++) {
+	console.log(testStudent1.scheduleBlockList[i].location);
+}
 
 /* Populate Test Student */
 function populateScheduleBlocks(scheduleBlockList) {
@@ -24,31 +29,31 @@ function populateScheduleBlocks(scheduleBlockList) {
 
 	block = {};
 
-	block['starttime'] = 1300;
-	block['endtime'] = 1400;
+	block['starttime'] = 1230;
+	block['endtime'] = 1300;
 	block['name'] = "Cool Class 1";
 	block['id'] = "COOL1234";
-	block['location'] = "Goldberg";
+	block['location'] = "Killam";
 
 	scheduleBlockList.push(block);
 
 	block = {};
 
-	block['starttime'] = 1300;
-	block['endtime'] = 1400;
+	block['starttime'] = 800;
+	block['endtime'] = 1100;
 	block['name'] = "Cool Class 1";
 	block['id'] = "COOL1234";
-	block['location'] = "Goldberg";
+	block['location'] = "Dunn";
 
 	scheduleBlockList.push(block);
 
 	block = {};
 
-	block['starttime'] = 1300;
-	block['endtime'] = 1400;
+	block['starttime'] = 1400;
+	block['endtime'] = 1600;
 	block['name'] = "Cool Class 1";
 	block['id'] = "COOL1234";
-	block['location'] = "Goldberg";
+	block['location'] = "Mona Campbell";
 
 	scheduleBlockList.push(block);
 }
