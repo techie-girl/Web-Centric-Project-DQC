5c2397d9694189eabb23d8ed03e779528d73d63b
Matthew <matthewjames.ns@googlemail.com>
2017-11-25 14:27:34 -0400

    Schedule block drawing and schedule toggle logic implementation.

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,8 @@
 /* List of Students */
 var studentList = [];
 
+var schedulesDisplayed = 1;
+
 /* Test Students */
 var testStudent1 = {
 	name: 'Matthew Duggan',
@@ -9,6 +11,13 @@ var testStudent1 = {
 	b00: 'B00617196'
 };
 
+var testStudent2 = {
+	name: 'Hawley Jean',
+	blockCounter: 0,
+	scheduleBlockList: [],
+	b00: 'B00626007'
+}
+
 /* On load */
 window.onload = populateSchedule;
 
@@ -16,21 +25,74 @@ function populateSchedule () {
 	populateScheduleBlocks(testStudent1.scheduleBlockList);
 	studentList.push(testStudent1);
 	drawBlocks(testStudent1);
+
+	populateScheduleBlocks2(testStudent2.scheduleBlockList);
+	studentList.push(testStudent2);
+	drawBlocks(testStudent2);
 }
 
 function toggleSchedule(name) {
 	var clickedName = name.replace(/\n|<.*?>/g,'');
 	var reducedName = clickedName.replace(/\s/g, '');
 	var counter = 0;
+	var displayingSched = false;
 
 	while(document.getElementById(reducedName + counter) != null) {
-		console.log("hit!" + counter);
-		if (document.getElementById(reducedName + counter).style.opacity === '0')
+		if (document.getElementById(reducedName + counter).style.opacity === '0') {
 			document.getElementById(reducedName + counter).style.opacity = "100";
-		else
+
+			displayingSched = true;
+
+			newEl = document.getElementById(reducedName + counter)
+
+			newEl.classList.remove('block-box-blue');
+			newEl.classList.remove('block-box-pink');
+			newEl.classList.remove('block-box-amber');
+			newEl.classList.remove('block-box-black');
+			newEl.classList.remove('block-box-purple');
+			newEl.classList.remove('block-box-brown');
+			newEl.classList.remove('block-box-red');
+			newEl.classList.remove('block-box-green');
+
+			switch(schedulesDisplayed) {
+				case 1:
+					newEl.classList.add('block-box-blue');
+					break;
+				case 2:
+					newEl.classList.add('block-box-pink');
+					break;
+				case 3:
+					newEl.classList.add('block-box-amber');
+					break;
+				case 4:
+					newEl.classList.add('block-box-black');
+					break;
+				case 5:
+					newEl.classList.add('block-box-purple');
+					break;
+				case 6:
+					newEl.classList.add('block-box-brown');
+					break;
+				case 7:
+					newEl.classList.add('block-box-red');
+					break;
+				case 8:
+					newEl.classList.add('block-box-green');
+					break;
+				default:
+					console.log("Error: Too many schedules displayed.");
+			}
+		}
+		else {
 			document.getElementById(reducedName + counter).style.opacity = "0";
+		}
 		counter++;
 	}
+
+	if (displayingSched) 
+			schedulesDisplayed++;
+	else 
+		schedulesDisplayed--;
 }
 
 /* Draw all schedule blocks from a student object */
@@ -44,9 +106,6 @@ function drawBlocks(student) {
 
 		var newEl = document.getElementById(name + student.b00 + student.blockCounter);
 		student.blockCounter++;
-		
-		console.log(student.scheduleBlockList[i].day);
-		console.log(student.scheduleBlockList[i].starttime)
 
 		switch(student.scheduleBlockList[i].day) {
 			case "Monday":
@@ -166,13 +225,54 @@ function drawBlocks(student) {
 				newEl.classList.add('twentythreethirty');
 				break;
 			default:
-				console.log("Error, unable to properly draw block.")
+				console.log("Error, unable to properly draw block.");
 
 		}
 
-		newEl.classList.add('block-half-hour');
-		newEl.classList.add('block-box-blue');
-		
+		var starttimeconvert, endtimeconvert;
+
+		if (student.scheduleBlockList[i].starttime%100 != 0) 
+			starttimeconvert = student.scheduleBlockList[i].starttime + 20;
+		else 
+			starttimeconvert = student.scheduleBlockList[i].starttime
+
+		if (student.scheduleBlockList[i].endtime%100 != 0) 
+			endtimeconvert = student.scheduleBlockList[i].endtime + 20;
+		else 
+			endtimeconvert = student.scheduleBlockList[i].endtime;
+
+
+		var timeframe = endtimeconvert - starttimeconvert;
+
+		switch(timeframe) {
+			case 50:
+				newEl.classList.add('block-half-hour');
+				break;
+			case 100:
+				newEl.classList.add('block-hour');
+				break;
+			case 150:
+				newEl.classList.add('block-hour-half');
+				break;
+			case 200:
+				newEl.classList.add('block-two-hours');
+				break;
+			case 250:
+				newEl.classList.add('block-two-half');
+				break;
+			case 300:
+				newEl.classList.add('block-three-hours');
+				break;
+			case 350:
+				newEl.classList.add('block-three-half');
+				break;
+			case 400:
+				newEl.classList.add('block-four-hours');
+				break;
+			default:
+				console.log("Error, unable to properly draw block.");
+		}
+
 		newEl.style.opacity = "0";
 	}
 }
@@ -231,17 +331,6 @@ function populateScheduleBlocks(scheduleBlockList) {
 	block['id'] = "CSCI4176";
 	block['location'] = "LSC";
 	block['day'] = "Tuesday";
-
-	scheduleBlockList.push(block);
-
-	block = {};
-
-	block['starttime'] = 830;
-	block['endtime'] = 1130;
-	block['name'] = "Mobile Computing";
-	block['id'] = "CSCI4176";
-	block['location'] = "Goldberg";
-	block['day'] = "Wednesday";
 
 	scheduleBlockList.push(block);
 
@@ -318,6 +407,145 @@ function populateScheduleBlocks(scheduleBlockList) {
 	block['name'] = "Web Centric Computing";
 	block['id'] = "CSCI3172";
 	block['location'] = "Rowe";
+	block['day'] = "Friday";
+
+	scheduleBlockList.push(block);
+}
+
+/* Populate Test Student */
+function populateScheduleBlocks2(scheduleBlockList) {
+	var block = {};
+
+	block['starttime'] = 1030;
+	block['endtime'] = 1130;
+	block['day'] = "Monday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1130;
+	block['endtime'] = 1230;
+	block['day'] = "Monday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1330;
+	block['endtime'] = 1430;
+	block['day'] = "Monday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1730;
+	block['endtime'] = 1830;
+	block['day'] = "Monday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1130;
+	block['endtime'] = 1230;
+	block['day'] = "Tuesday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1030;
+	block['endtime'] = 1130;
+	block['day'] = "Wednesday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1130;
+	block['endtime'] = 1230;
+	block['day'] = "Wednesday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1330;
+	block['endtime'] = 1430;
+	block['day'] = "Wednesday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1730;
+	block['endtime'] = 1830;
+	block['day'] = "Wednesday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1030;
+	block['endtime'] = 1130;
+	block['day'] = "Thursday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1130;
+	block['endtime'] = 1230;
+	block['day'] = "Thursday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1730;
+	block['endtime'] = 2030;
+	block['day'] = "Thursday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1030;
+	block['endtime'] = 1130;
+	block['day'] = "Friday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1130;
+	block['endtime'] = 1230;
+	block['day'] = "Friday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1330;
+	block['endtime'] = 1430;
+	block['day'] = "Friday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1600;
+	block['endtime'] = 1730;
+	block['day'] = "Friday";
+
+	scheduleBlockList.push(block);
+
+	block = {};
+
+	block['starttime'] = 1730;
+	block['endtime'] = 1830;
 	block['day'] = "Friday";
 
 	scheduleBlockList.push(block);
