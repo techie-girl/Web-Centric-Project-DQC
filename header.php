498e79b508e03e866c933967866a42b253d16a64
stephanieconnor <stephanieconnor1@gmail.com>
2017-11-29 23:35:23 -0400

    Added CSS
    added a header.php file and updated some CSS

---
+++ header.php
@@ -0,0 +1,41 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- JavaScript -->
+  <script src="js/logic.js"></script>
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/styles.css">
+
+    <!-- google fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+</head>
+<body>
+
+<header id="banner">
+
+  <img src="images/dal-logo.png" alt="dalhousie university logo">
+
+  <h1>Dalhousie Quick Collab</h1>
+</header>
+
+
+  <nav id="sec"> 
+    <ul>
+      <li><a href="#"><strong>HELP |</strong></a></li>
+      <li><a href="#"><strong> EXIT</strong></a></li>
+    </ul>
+  </nav>
+
+  <nav id="main">
+    <ul>
+      <li><a href="home.html">Home</a></li>
+      <li><a href="index.html">Student</a></li>
+      <li><a href="#">Schedule</a></li>
+      <li><a href="#">Groups</a></li>
+    </ul>
+  </nav>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+include('header.php'); ?>
+
+<h2>Schedule</h2>  
+
+<h4> Students in Schedule: </h4>
+<ul id="current-student-list"> 
+</ul>
+
+<div id="schedule-container">
+  <div class="time-column">
+
+      <h3>Monday</h3>
+
+  </div>
+
+  <div class="time-column">
+
+      <h3>Tuesday</h3>
+
+  </div>
+
+  <div class="time-column">
+
+      <h3>Wednesday</h3>
+
+  </div>
+
+  <div class="time-column">
+
+      <h3>Thursday</h3>
+
+  </div>
+
+  <div class="time-column" id="last-tc">
+
+      <h3>Friday</h3>
+
+  </div>
+
+  <img src="images/scheduleview.PNG" alt="Schedule View" width="550px">
+
+</div>
+
+<div id="side-content">
+  <div id="students-container">
+    <h2> Students </h2>
+    <div id="student-list">
+      <ul id="students-ul">
+      </ul>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
